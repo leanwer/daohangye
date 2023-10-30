d79cf69e9b4be37951ff291e6fb236fe50685179
leanwer <42107941+leanwer@users.noreply.github.com>
2023-10-30 17:36:26 +0800

    Update data3.js

--- js/data3.js
+++ js/data3.js
@@ -1,9 +1,9 @@
 const data = [
   { name: "图片素材下载", link: "https://www.pexels.com/", image:"pexels.jpg" },
-  { name: "图片素材下载（备用）", link: "https://pixabay.com/", image:"pixabay.png" },
-  { name: "字节的可商用图标下载", link: "https://iconpark.oceanengine.com/", image:"iconpark.png" },
-  { name: "图标下载（非商用）", link: "https://www.iconfont.cn/", image:"iconfont.png" },
-  { name: "日本的矢量免费素材", link: "https://illustac-cn.com/", image:"ac-illust.jpg" },
-  { name: "3D素材下载（会员）", link: "https://www.adasc.net/", image:"cyj.jpg" },
-  { name: "hdr,材质,模型(3D)", link: "https://polyhaven.com/models", image:"plhv.jpg" },
+  { name: "图片素材下载", link: "https://pixabay.com/", image:"pixabay.png" },
+  { name: "商用图标下载", link: "https://iconpark.oceanengine.com/", image:"iconpark.png" },
+  { name: "图标", link: "https://www.iconfont.cn/", image:"iconfont.png" },
+  { name: "矢量免费素材", link: "https://illustac-cn.com/", image:"ac-illust.jpg" },
+  { name: "3D素材", link: "https://www.adasc.net/", image:"cyj.jpg" },
+  { name: "hdr,材质,模型", link: "https://polyhaven.com/models", image:"plhv.jpg" },
 ]
